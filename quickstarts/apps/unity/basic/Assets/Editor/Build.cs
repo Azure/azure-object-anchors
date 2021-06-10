5fa297ede7ca157207efcf019b3ea85f0fff555a
Craig Treasure <crtreasu@microsoft.com>
2021-06-10 11:49:49 -0700

    Samples update for June 2021
      - Updated the AOA Unity SDK to 0.11.0.
      - Added a new VisualizeScene to the Basic Unity sample.

--- quickstarts/apps/unity/basic/Assets/Editor/Build.cs
+++ quickstarts/apps/unity/basic/Assets/Editor/Build.cs
@@ -1,7 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 using System.Linq;
+using System.IO;
 using UnityEditor;
+using System;
 
 namespace Microsoft.Azure.ObjectAnchors.Unity
 {
@@ -12,15 +14,7 @@ namespace Microsoft.Azure.ObjectAnchors.Unity
         /// </summary>
         public static void GenerateHoloLensPlayerSolution()
         {
-            EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.WSA, BuildTarget.WSAPlayer);
-            EditorUserBuildSettings.SetPlatformSettings("WindowsStoreApps", "CopyReferences", "true");
-            EditorUserBuildSettings.SetPlatformSettings("WindowsStoreApps", "CopyPDBFiles", "false");
-
-            EditorUserBuildSettings.wsaUWPVisualStudioVersion = "Visual Studio 2019";
-            EditorUserBuildSettings.wsaUWPSDK = "10.0.18362.0";
-            EditorUserBuildSettings.wsaSubtarget = WSASubtarget.HoloLens;
-
-            PlayerSettings.SetScriptingBackend(BuildTargetGroup.WSA, ScriptingImplementation.IL2CPP);
+            ConfigureCoreSettings();
 
             BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions()
             {
@@ -35,6 +29,38 @@ namespace Microsoft.Azure.ObjectAnchors.Unity
             };
 
             BuildPipeline.BuildPlayer(buildPlayerOptions);
+        }
+
+        public static void GenerateVisualizerSolution()
+        {
+            ConfigureCoreSettings();
+
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions()
+            {
+                locationPathName = "UWP_visual",
+                target = BuildTarget.WSAPlayer,
+                targetGroup = BuildTargetGroup.WSA,
+                options = BuildOptions.None,
+                scenes = EditorBuildSettings.scenes
+                         .Where(scene => Path.GetFileName(scene.path).Equals("VisualizeScene.unity", StringComparison.OrdinalIgnoreCase))
+                         .Select(scene => scene.path)
+                         .ToArray(),
+            };
+
+            BuildPipeline.BuildPlayer(buildPlayerOptions);
+        }
+
+        private static void ConfigureCoreSettings()
+        {
+            EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.WSA, BuildTarget.WSAPlayer);
+            EditorUserBuildSettings.SetPlatformSettings("WindowsStoreApps", "CopyReferences", "true");
+            EditorUserBuildSettings.SetPlatformSettings("WindowsStoreApps", "CopyPDBFiles", "false");
+
+            EditorUserBuildSettings.wsaUWPVisualStudioVersion = "Visual Studio 2019";
+            EditorUserBuildSettings.wsaUWPSDK = "10.0.18362.0";
+            EditorUserBuildSettings.wsaSubtarget = WSASubtarget.HoloLens;
+
+            PlayerSettings.SetScriptingBackend(BuildTargetGroup.WSA, ScriptingImplementation.IL2CPP);
         }
     }
 }
---
+++ quickstarts/apps/unity/basic/Assets/MixedReality.AzureObjectAnchors/Scripts/MeshLoader.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.IO;
+using Numerics = System.Numerics;
+
+using Microsoft.Azure.ObjectAnchors;
+using Microsoft.Azure.ObjectAnchors.Unity;
+using System.Linq;
+using System;
+
+public class MeshLoader : MonoBehaviour
+{
+    public string ModelPath;
+
+    async void Start()
+    {
+        Debug.Log($"Loading model from: '{ModelPath}'");
+
+        MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+        Mesh mesh = new Mesh();
+
+        ObjectObserver observer = new ObjectObserver();
+        byte[] modelBytes = File.ReadAllBytes(ModelPath);
+        ObjectModel model = await observer.LoadObjectModelAsync(modelBytes);
+
+        gameObject.transform.localPosition = model.BoundingBox.Center.ToUnity();
+        gameObject.transform.localRotation = model.BoundingBox.Orientation.ToUnity();
+
+        // We need to flip handedness of vertices and modify triangle list to
+        // clockwise winding in order to be usable in Unity.
+
+        Numerics.Vector3[] modelVertices = new Numerics.Vector3[model.VertexCount];
+        model.GetVertexPositions(modelVertices);
+        mesh.vertices = modelVertices.Select(v => v.ToUnity()).ToArray();
+        if (modelVertices.Length > UInt16.MaxValue)
+        {
+            mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+        }
+
+        if (model.TriangleIndexCount > 2)
+        {
+            // Counter clock wise
+            uint[] modelIndices_ccw = new uint[model.TriangleIndexCount];
+            model.GetTriangleIndices(modelIndices_ccw);
+
+            // Clock wise
+            int[] modelIndices_cw = new int[modelIndices_ccw.Length];
+            Enumerable.Range(0, modelIndices_ccw.Length).Select(i =>
+                i % 3 == 0 ?
+                modelIndices_cw[i] = (int)modelIndices_ccw[i] : i % 3 == 1 ?
+                modelIndices_cw[i] = (int)modelIndices_ccw[i + 1] : modelIndices_cw[i] = (int)modelIndices_ccw[i - 1])
+                .ToArray();
+
+            mesh.SetIndices(modelIndices_cw, MeshTopology.Triangles, 0);
+        }
+        else
+        {
+            mesh.SetIndices(Enumerable.Range(0, modelVertices.Length).ToArray(), MeshTopology.Points, 0);
+        }
+
+        Numerics.Vector3[] modelNormals = new Numerics.Vector3[model.VertexCount];
+        model.GetVertexNormals(modelNormals);
+        mesh.normals = modelNormals.Select(v => v.ToUnity()).ToArray();
+
+        mesh.RecalculateBounds();
+        meshFilter.mesh = mesh;
+    }
+}
